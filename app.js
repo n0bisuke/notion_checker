8c14f4946973165a127a76d2cd39951871f76bca
n0bisuke <rsksound@gmail.com>
2022-12-07 21:03:32 +0900

    update

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 require('dotenv').config()
 
 const NotionAPI = require('./module/db.js');
+const discrod = require('./module/discord.js');
 const nClient = new NotionAPI();
 
 const dayjs = require('dayjs');
-dayjs.extend(require('dayjs/plugin/timezone'))
-dayjs.extend(require('dayjs/plugin/utc'))
-dayjs.tz.setDefault('Asia/Tokyo')
-
-const now = dayjs(); // 現在の日付情報を取得
-console.log(`現在時間`,now.tz().format('YYYY-MM-DD HH:mm:ss'));
+dayjs.extend(require('dayjs/plugin/timezone'));
+dayjs.extend(require('dayjs/plugin/utc'));
+dayjs.tz.setDefault('Asia/Tokyo');
+dayjs.extend(require('dayjs/plugin/isBetween'));
 
 // Initializing a client
 const main = async () => {
     const items = await nClient.getDBbyClass('po-07');
-    // console.log(items.properties.Name);
 
     const users = [];
     for await (const item of items) {
@@ -35,9 +33,30 @@ const main = async () => {
       updateList.push(item);
     }
 
-    const editTimeJP = dayjs(updateList[1].block.last_edited_time).tz().format('YYYY-MM-DD HH:mm:ss');
-    // console.log()
-    console.log(`[${updateList[1].studentName}]さんの最終更新は${editTimeJP}です。 内容は「${updateList[1].block.text}」`);
+    //時間を確認
+    const now = dayjs(); // 現在の日付情報を取得
+    const currentTime = now.tz().format('YYYY-MM-DD HH:mm:ss');
+    const towHAgo = dayjs(currentTime).subtract(2, 'h').format(); //2時間前の時間
+    console.log(`現在時間:`,currentTime);
+    console.log(`2時間前：`, towHAgo);
+
+    for (update of updateList){
+      
+      const editTimeJP = dayjs(update.block.last_edited_time).tz().format('YYYY-MM-DD HH:mm:ss');
+      if(dayjs(editTimeJP).isBetween(towHAgo, currentTime)){
+        console.log(`2時間以内の変更:`);
+        const text = `[${update.studentName}]さんの最終更新は${editTimeJP}です。 内容は「${update.block.text}」`;
+        const discordPostData = {
+            username: 'Notion通知',
+            content: text
+        }
+        await discrod(discordPostData);
+        console.log(text);
+      }else{
+        console.log(`それ以外:`);
+      }
+
+    }
 }
 
 main();
---
+++ module/discord.js
@@ -0,0 +1,28 @@
+'use strict'
+
+require('dotenv').config()
+const URL = process.env.DISCORD_WEBHOOK_URL;
+
+const main = async (postData) => {
+    //送信するデータ
+    // const postData = {
+    //     username: 'Notion通知',
+    //     content: 'Node.js Fetch APIからポスト'
+    // }
+    const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData)
+    });
+
+    // const json = await response.json();
+    console.log(response.status);
+    return response.status;
+}
+
+module.exports = main;
+
+// main();
